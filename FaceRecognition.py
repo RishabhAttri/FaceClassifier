70efe2b15bb18a3cc18f31129c15b824b5bfe61a
RishabhAttri <rishabhattri2308@gmail.com>
2019-12-12 19:40:49 +0530

    Wrote the KNN

---
+++ FaceRecognition.py
@@ -0,0 +1,48 @@
+# Collect Data from data folder
+# Store the images from live webcam 
+# Apply KNN algorithm to check for the closest match
+
+from os import listdir
+import numpy as np
+
+path = './data/'
+dir_list = listdir(path)
+print("files in data folder",dir_list)
+names = []
+data = []
+for person in dir_list:
+    if person.endswith('.npy'):
+        names.append(person[:-4])
+        data_ary = np.load(path+person, allow_pickle=True)
+        data.append(data_ary)
+data_array = np.array(data)
+# print(data_array)
+print(data_array.shape)
+for person in data_array:
+    print(person.shape)
+# Data array would be like (No of people in the data folder, No of entries(variable), 30000)
+# Take Data from the webCam take data like (rows, 30000)
+# Each row to be processed all together 
+# Apply KNN on each row and the most closest item to be returned
+
+def KNN(input_row, k):
+    pred = []
+    match_rows = []
+    for i in range(data_array.shape[0]):
+        dist = 0
+        for targetRow in data_array[i]:
+            for i in range(targetRow.size):
+                dist += abs((input_row[i]-targetRow[i]))
+        dist = dist/data_array[i].shape[0]
+        match_rows.append((dist,i))
+        match_rows = sorted(match_rows)
+        match_rows = match_rows[:k]
+        pred = match_rows[:,1]
+        print(pred)
+        return pred
+
+        
+
+    
+
+
